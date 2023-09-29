3636a4aa0c005ebe9650f5f75f79f0129a33526e
Eugene Owak <teugeneowak@gmail.com>
2023-09-29 13:19:47 +0300

    Change ibl highlight.

--- lua/cobalt2/plugins/indent-blankline.lua
+++ lua/cobalt2/plugins/indent-blankline.lua
@@ -2,4 +2,4 @@ local colors = require("cobalt2.utils").colors
 local Group = require("cobalt2.utils").Group
 
 Group.new("IblIndent", colors.darker_grey:light(), nil, nil)
-Group.new("IndentBlankLineContextChar", colors.yellow, nil, nil)
+Group.new("IblScope", colors.yellow, nil, nil)
